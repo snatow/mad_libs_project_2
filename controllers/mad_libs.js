e9463ca97cb7aeefe6bccb40084772a85752c191
snatow <natow.sarah@gmail.com>
2016-05-31 15:32:58 -0400

    adding wireframes

--- controllers/mad_libs.js
+++ controllers/mad_libs.js
@@ -20,35 +20,40 @@ router.get('/', function(req, res) {
   })
 });
 
-//New Page Pirate Mad Lib
+//NEW PAGE
+// "Pirate" Mad Lib 
 router.get("/newpirate", function(req, res){
   // console.log('hello');
   // res.send('goodbye')
   res.render("./newPages/pirate.ejs");
 });
 
-//New Page Date Mad Lib
+//NEW PAGE
+// "Date" Mad Lib
 router.get("/newdate", function(req, res){
   // console.log('hello');
   // res.send('goodbye')
   res.render("./newPages/date.ejs");
 });
 
-//New Page Study Mad Lib
+//NEW PAGE
+// "Study" Mad Lib
 router.get("/newstudy", function(req, res){
   // console.log('hello');
   // res.send('goodbye')
   res.render("./newPages/study.ejs");
 });
 
-//New Page Dentist Mad Lib
+//NEW PAGE
+// "Dentist" Mad Lib
 router.get("/newdentist", function(req, res){
   // console.log('hello');
   // res.send('goodbye')
   res.render("./newPages/dentist.ejs");
 });
 
-// Show Page Pirate Mad Lib
+// SHOW PAGE 
+// "Pirate" Mad Lib 
 router.get('/pirate/:id', function(req, res) {
   console.log(req.params.id);
   Form.findById(req.params.id).then(function(form) {
@@ -57,7 +62,8 @@ router.get('/pirate/:id', function(req, res) {
   });
 });
 
-// Show Page Date Mad Lib
+// SHOW PAGE  
+// "Date" Mad Lib
 router.get('/date/:id', function(req, res) {
   console.log(req.params.id);
   Form.findById(req.params.id).then(function(form) {
@@ -66,7 +72,8 @@ router.get('/date/:id', function(req, res) {
   });
 });
 
-// Show Page Study Mad Lib
+// SHOW PAGE
+// "Study" Mad Lib
 router.get('/study/:id', function(req, res) {
   console.log(req.params.id);
   Form.findById(req.params.id).then(function(form) {
@@ -75,7 +82,8 @@ router.get('/study/:id', function(req, res) {
   });
 });
 
-// Show Page Dentist Mad Lib
+// SHOW PAGE
+// "Dentist" Mad Lib
 router.get('/dentist/:id', function(req, res) {
   // console.log("hello");
   // console.log(req.params.id);
@@ -85,35 +93,40 @@ router.get('/dentist/:id', function(req, res) {
   });
 });
 
-//Edit Page Pirate Mad Lib
+//EDIT PAGE
+// "Pirate" Mad Lib 
 router.get("/pirate/:id/edit", function(req, res) {
   Form.findById(req.params.id).then(function(form) {
     res.render("./editPages/pirate.ejs", form);
   });
 });
 
-//Edit Page Date Mad Lib
+//EDIT PAGE
+// "Date" Mad Lib
 router.get("/date/:id/edit", function(req, res) {
   Form.findById(req.params.id).then(function(form) {
     res.render("./editPages/date.ejs", form);
   });
 });
 
-//Edit Page Study Mad Lib
+//EDIT PAGE
+// "Study" Mad Lib
 router.get("/study/:id/edit", function(req, res) {
   Form.findById(req.params.id).then(function(form) {
     res.render("./editPages/study.ejs", form);
   });
 });
 
-//Edit Page Dentist Mad Lib
+//EDIT PAGE
+// "Dentist" Mad Lib
 router.get("/dentist/:id/edit", function(req, res) {
   Form.findById(req.params.id).then(function(form) {
     res.render("./editPages/dentist.ejs", form);
   });
 });
 
-//Update Page 
+//UPDATE PAGE
+//This route works for all four mad libs
 router.put("/:id", function(req, res) {
   // console.log(req.body);
   // console.log(req.params.id); 
@@ -122,13 +135,18 @@ router.put("/:id", function(req, res) {
   })
 })
 
-// Create
+// CREATE PAGE
+//This route works for all four mad libs
 router.post('/', function(req, res) {
+  // this variable will serve to populate images for the show pages
   var word = req.body.title;
+  //this callback function provides my Key and Secret to the Flickr API
   flickr.tokenOnly(flickrOptions, function(error, flickr) {
+    //this callback function tests the connection to the Flickr API
     flickr.test.echo({"test": "test"}, function(err,result) {
       if(err) { return console.log("note: error connecting to the flickr API"); }
       var noun1Result;
+      //this function searches Flickr based on the key word for each mad lib
       flickr.photos.search({ 
         tags: word,
         per_page: 4,
@@ -136,6 +154,7 @@ router.post('/', function(req, res) {
         if(err) { return console.log("error:", err); }
         console.log(result.photos.photo.length + " results found. First result:");
         // console.log("this is the result in their code: " + JSON.stringify(result.photos.photo[0],false,2));
+        //This block of variables takes the output from the Flickr API and parses it into a form that can be saved in the database and used to render the images on screen
         var firstResult = result.photos.photo[0];
         var img1 = "https://farm" + firstResult.farm + ".staticflickr.com/" + firstResult.server + "/" + firstResult.id + "_" + firstResult.secret + ".jpg"
         req.body.image1 = img1;
@@ -149,6 +168,7 @@ router.post('/', function(req, res) {
         var img4 = "https://farm" + fourthResult.farm + ".staticflickr.com/" + fourthResult.server + "/" + fourthResult.id + "_" + fourthResult.secret + ".jpg"
         req.body.image4 = img4;
         // console.log(req.body);
+        //this variable and function allow me to save the new data to the database
         var newform = new Form(req.body);
         newform.save(function(err) {
           if(err) {
@@ -165,7 +185,7 @@ router.post('/', function(req, res) {
   });
 });
 
-// // Create
+// // Create - this is a pre API test route
 // router.post('/', function(req, res) {
 //   // console.log(req.body);
 //   // res.send("post request");
@@ -180,6 +200,7 @@ router.post('/', function(req, res) {
 // });
 
 // Delete
+// This route works for all four mad libs
 router.delete('/:id', function(req, res) {
   Form.findOneAndRemove({_id: req.params.id}, function(err) {
     if(err) console.log(err);
