5072f37989013290f1115c28423b17ee09ef260d
snatow <natow.sarah@gmail.com>
2016-05-31 13:48:40 -0400

    tweaking routes

---
+++ readme.mk
@@ -0,0 +1,18 @@
+## Mad Libs With Pictures
+
+[Mad_Libs_With_Pictures](http://mad-libs-pics.herokuapp.com/mad-libs)
+
+### Description
+Mad Libs is a template word game where one player prompts others for a list of words to substitute for blanks in a story, before reading the – often comical or nonsensical – story aloud. This web app rendition of the game populates the story for the players and decorates the page with four images pulled at random from Flickr that are thematically related to the content of the story. 
+
+### Tech Used
+- HTML
+- CSS
+- EJS
+- Node.js
+- Flickr API
+
+### Features
+- Users are given a choice of four different mad libs from which they can choose by clicking on their titles
+- Once clicked, these links will route the User to a form with inputs for all of the missing words labeled with their parts of speech. Once the form is completed, the user will click the Submit button to add their version of the mad lib to the database. This will bring the user to the show page for this newly created mad lib.
+- Users can view previously created and saved mad libs from the index page of the app, edit those mad libs from their show pages and save those edits, and delete them from the database on the individual page's edit page. 
